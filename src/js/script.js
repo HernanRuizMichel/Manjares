ee58bfda9cdd06e8ce373c7e67b6a20b11ac9ff7
HernanRuizMichel <hagohermichel@gmail.com>
2023-01-07 13:05:34 -0400

    Añadiendo lista de links menu hambuerguesa

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,11 @@ menuBar.addEventListener("click", ()=>{
     document.querySelector(".line1__menu--bar").classList.toggle("activateline1__menu--bar");
     document.querySelector(".line2__menu--bar").classList.toggle("activateline2__menu--bar");
     document.querySelector(".line3__menu--bar").classList.toggle("activateline3__menu--bar");
+    document.querySelector(".lista__menu").classList.toggle("activatelista__menu");
+    if (document.querySelector(".activatelista__menu")) {
+        document.querySelector(".main").style.opacity = "0.8";
+    } else {
+        document.querySelector(".main").style.opacity = "1";
+    }
 })
 // Menu hambuerguesa
