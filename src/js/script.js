ca0ffe30d1b9517ca04c545703132a0c3f20d239
HernanRuizMichel <hagohermichel@gmail.com>
2023-01-15 18:21:16 -0400

    Aadiendo cartas de productos

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,17 @@ menuBar.addEventListener("click", ()=>{
     document.querySelector(".line3__menu--bar").classList.toggle("activateline3__menu--bar");
     document.querySelector(".lista__menu").classList.toggle("activatelista__menu");
     if (document.querySelector(".activatelista__menu")) {
-        document.querySelector(".main").style.opacity = "0.8";
+        document.querySelector(".main").style.opacity = "0.6";
+        document.querySelector(".main").style.zIndex = "-1";
+        window.addEventListener('scroll', disableScroll);
     } else {
         document.querySelector(".main").style.opacity = "1";
+        document.querySelector(".main").style.zIndex = "1";
+        window.removeEventListener('scroll', disableScroll);
     }
 })
+
+function disableScroll(){  
+    window.scrollTo(0, 0);
+  }
 // Menu hambuerguesa
